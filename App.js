5953a1af9148309527fef4e1787d1050b08e8245
seras <109229384+seras42@users.noreply.github.com>
2023-08-30 17:38:14 +0300

    init

---
+++ App.js
@@ -0,0 +1,71 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+  
+
+    <View style={[styles.container, {
+      flexDirection: 'column',
+    },
+    ]}>
+
+      <View style={styles.titleContainer}>
+
+      <Text style={styles.title}>To Do</Text>
+    </View>
+
+    <View style = {styles.listContainer}>
+
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      </View>
+
+      <TouchableOpacity
+        title="Press me"
+        onPress={() => print('Simple Button pressed')}
+        style = {styles.button}>
+
+
+         <Text style = {{color: 'white', textAlign: 'center', fontSize: 30}}>+</Text>
+
+      </TouchableOpacity>
+      
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 30
+  },
+  button: {
+    backgroundColor: 'green',
+    color: 'white',
+    padding: 20,
+    textAlign: 'center',
+    fontSize: 100,
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+  titleContainer: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: 'grey',
+    flexDirection: 'column',
+    borderRadius: 8,
+    padding: 20,
+    minHeight: 80
+  
+
+   
+  },
+  listContainer: {
+
+  }
+});
