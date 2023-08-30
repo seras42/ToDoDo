528e85f57994b6247e645f30630caa011d4a83e6
seras <109229384+seras42@users.noreply.github.com>
2023-08-30 19:02:07 +0300

    Fixed buttons

--- App.js
+++ App.js
@@ -5,38 +5,52 @@ export default function App() {
   return (
   
 
-    <View style={[styles.container, {
-      flexDirection: 'column',
-    },
-    ]}>
+    <View>
 
-      <View style={styles.titleContainer}>
-
-      <Text style={styles.title}>To Do</Text>
-    </View>
-
+      <View style={styles.container}>
+      <TextTitle />
     <View style = {styles.listContainer}>
-
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /></View>
+      
       </View>
 
+
+
+      <CreateNewTaskButton />
+    </View>
+  );
+}
+const CreateNewTaskButton = () => {
+  return (
+<View style={styles.buttonContainer}>
       <TouchableOpacity
         title="Press me"
         onPress={() => print('Simple Button pressed')}
-        style = {styles.button}>
+        style = {styles.button}
+        >
 
 
          <Text style = {{color: 'white', textAlign: 'center', fontSize: 30}}>+</Text>
 
-      </TouchableOpacity>
-      
-    </View>
-    
+      </TouchableOpacity></View>
   );
-}
+};
+
+const TextTitle = () => {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>To Do</Text></View>
+  );
+};
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignContent: 'center',
+    height:'100%'
+    
+  },
   title: {
     color: 'black',
     fontWeight: 'bold',
@@ -47,25 +61,29 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     color: 'white',
     padding: 20,
-    textAlign: 'center',
-    fontSize: 100,
     borderRadius: 20,
     paddingVertical: 10,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
+    width: 60,
+    height: 60
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 10,
+    alignItems: 'flex-end',
+    right:10
+
   },
   titleContainer: {
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     backgroundColor: 'grey',
-    flexDirection: 'column',
     borderRadius: 8,
     padding: 20,
-    minHeight: 80
+    minHeight: 80,
   
 
    
-  },
-  listContainer: {
-
   }
+  
 });
